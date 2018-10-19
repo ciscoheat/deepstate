477f1be2721c4cc354dafebd16d95d24c77c2628
Andreas <ciscoheat@gmail.com>
2018-10-20 00:20:27 +0200

    ImmutableArray/Map update.

--- src/ds/ImmutableArray.hx
+++ src/ds/ImmutableArray.hx
@@ -1,21 +1,40 @@
 package ds;
 
 @:forward(length, concat, copy, filter, indexOf, iterator, join, lastIndexOf, map, slice, splice, toString)
-abstract ImmutableArray<T>(Array<T>) {
+abstract ImmutableArray<T>(Array<T>) to Iterable<T> {
 	@:arrayAccess inline function arrayAccess(key : Int) : T 
         return this[key];
 
     inline function new(array : Array<T>)
         this = array;
 
+    ///// From/to conversions /////
+
     @:from public static function fromArray<T2>(array : Array<T2>) 
         return new ImmutableArray(array.copy());
 
-    @:to public function toIterable() : Iterable<T>
-        return this;
+//    @:to public function toIterable() : Iterable<T>
+//        return this;
 
     public function toArray() : Array<T>
-        return this;
+        return this.copy();
+
+    // TODO: This will probably cause object schizohprenia.
+    /*
+    @:to public function toAnon()
+    	return {
+            length: this.length, concat: this.concat, copy: copy, filter: this.filter, 
+            indexOf: this.indexOf, iterator: this.iterator, join: this.join, 
+            lastIndexOf: this.lastIndexOf, map: this.map, slice: this.slice, 
+            splice: this.splice, toString: this.toString,
+            insert: insert, pop: pop, push: push, remove: remove, reverse: reverse,
+            shift: shift, sort: sort, unshift: unshift, first: first, last: last
+        };
+    */
+
+    ///// Array API modifications /////
+
+    //public function copy() this.copy();
 
     public function insert(pos : Int, x : T) : ImmutableArray<T> {
         var newArray = this.copy();
--- src/ds/ImmutableMap.hx
+++ src/ds/ImmutableMap.hx
@@ -2,24 +2,21 @@ package ds;
 
 import haxe.ds.Map;
 
-@:forward(exists, get, iterator, keys, toString)
+@:multiType @:forward(exists, get, iterator, keys, toString)
 abstract ImmutableMap<K, V>(Map<K, V>) from Map<K, V> {
   	@:arrayAccess @:extern inline function arrayAccess(key:K):V return this.get(key);
 
-    public inline function new(map : Map<K, V>) this = map;
+    public function new(map : Map<K, V>); // this = map;
 
-    /*
     public function set(key : K, value : V) : ImmutableMap<K, V> {
-        var newMap : Map<K,V> = new Map(); 
-        for(k in this.keys()) newMap.set(k, this[k]);
+        var newMap : Map<K,V> = this.copy();
         newMap.set(key, value);
         return newMap;
     }
 
     public function remove(key : K) : ImmutableMap<K, V> {
-        return [for(k in this.keys()) 
-            if(key != k) k => this.get(k)
-        ];
+        var newMap : Map<K,V> = this.copy();
+        newMap.remove(key);
+        return newMap;
     }
-    */
 }
